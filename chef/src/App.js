80210deb49fc0acb37f6b5f4ab51c1d562b13934
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-21 10:43:46 -0600

    initial file structure

---
+++ chef/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>So it begins.....</h1>
+    </div>
+  );
+}
+
+export default App;
